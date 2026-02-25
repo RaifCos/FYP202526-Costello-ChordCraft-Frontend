6f06ce8ec68d6fdf3aa2720998455f31d0e435e0
RaifCos <RaifCostelloBusiness@gmail.com>
2026-02-25 13:21:17 +0000

    Create APIManager.kt

---
+++ Application/app/src/main/java/com/example/chordcraft/components/APIManager.kt
@@ -0,0 +1,11 @@
+package com.example.chordcraft.components
+
+import android.net.Uri
+
+fun callAPI(uri: Uri): String {
+    val apiURL = "https://fyp202526-costello-chordcraft-backend.onrender.com"
+    val apiEndpoint = "/run"
+    val result = ""
+    // TODO: Call API and save output to result variable.
+    return result
+}
