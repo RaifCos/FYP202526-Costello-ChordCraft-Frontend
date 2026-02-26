c5fde6072dea328a8b61c7f5062dd7adb7612792
RaifCos <RaifCostelloBusiness@gmail.com>
2026-02-26 14:41:07 +0000

    Add code to API Manager

--- Application/app/src/main/java/com/example/chordcraft/components/APIManager.kt
+++ Application/app/src/main/java/com/example/chordcraft/components/APIManager.kt
@@ -1,11 +1,24 @@
 package com.example.chordcraft.components
 
+import android.content.Context
 import android.net.Uri
+import okhttp3.*
+import okhttp3.RequestBody.Companion.toRequestBody
 
-fun callAPI(uri: Uri): String {
-    val apiURL = "https://fyp202526-costello-chordcraft-backend.onrender.com"
-    val apiEndpoint = "/run"
-    val result = ""
-    // TODO: Call API and save output to result variable.
-    return result
+
+fun callAPI(context: Context, uri: Uri): String {
+    val client = OkHttpClient()
+    val stream = context.contentResolver.openInputStream(uri) ?: return "Error: couldn't open file"
+
+    val requestBody = MultipartBody.Builder()
+        .setType(MultipartBody.FORM)
+        .addFormDataPart("file", uri.lastPathSegment ?: "audio", stream.readBytes().toRequestBody())
+        .build()
+
+    val request = Request.Builder()
+        .url(" https://fyp202526-costello-chordcraft-backend-production.up.railway.app/run")
+        .post(requestBody)
+        .build()
+
+    return client.newCall(request).execute().use { it.body?.string() ?: "Error: empty response" }
 }
